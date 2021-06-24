cf59c97569e46083ef1bcbf173749456bb5945a6
Daniela Shuman <dshuman@fb.com>
2021-06-24 16:33:10 -0400

    Initial commit

---
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -0,0 +1,39 @@
+package com.example.flixster;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.flixster.models.Movie;
+
+import org.parceler.Parcels;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    Movie movie;
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar rbVoteAverage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvOverview = (TextView) findViewById(R.id.tvOverview);
+        rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+
+        // Unwrap the movie data passed through the intent
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
+
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+
+        float voteAverage = movie.getVoteAverage().floatValue();
+        rbVoteAverage.setRating(voteAverage / 2.0f);
+    }
+}
---
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -0,0 +1,117 @@
+package com.example.flixster.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixster.MovieDetailsActivity;
+import com.example.flixster.R;
+import com.example.flixster.models.Movie;
+
+import org.jetbrains.annotations.NotNull;
+import org.parceler.Parcels;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    // Usually involves inflating a layout XML and returning the holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NotNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter", "onCreateViewHolder");
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(movieView);
+    }
+
+    // Populating the movie data into the Item Holder
+    @Override
+    public void onBindViewHolder(@NotNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter", "onBindViewHolder" + position);
+        Movie movie = movies.get(position);
+        holder.bind(movie);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+
+            // In order to acknowledge when a movie was clicked
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            String imageUrl;
+            int placeholder;
+            int width;
+            int height;
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imageUrl = movie.getBackdropPath();
+                placeholder = R.drawable.flicks_backdrop_placeholder;
+                width = 1600;
+                height = 900;
+            }
+            else {
+                imageUrl = movie.getPosterPath();
+                placeholder = R.drawable.flicks_movie_placeholder;
+                width = 200;
+                height = 300;
+            }
+            Glide.with(context)
+                .load(imageUrl)
+                //.centerCrop()
+                //.transform(new RoundedCornersTransformation(30, 0))
+                .placeholder(placeholder)
+                //.override(width,height)
+                .into(ivPoster);
+        }
+
+        @Override
+        public void onClick(View v) {
+            // Get the movie that was clicked
+            int position = getAdapterPosition();
+            // Ensures a valid position
+            if (position != RecyclerView.NO_POSITION) {
+                Movie movie = movies.get(position);
+                // Send data to the Movie Details Activity
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+                // Shows the Movie Details page
+                context.startActivity(intent);
+            }
+        }
+    }
+}
